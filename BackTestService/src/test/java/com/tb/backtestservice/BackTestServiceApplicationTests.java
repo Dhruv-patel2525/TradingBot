f01639eb710fca1436277162fae82106e2f70c5d
Dhruvipatel2525 <www.dp454@gmail.com>
2025-08-30 18:08:23 -0300

    Market DataService

---
+++ BackTestService/src/test/java/com/tb/backtestservice/BackTestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tb.backtestservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackTestServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
